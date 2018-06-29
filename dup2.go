8fb3808b512775625928db0db4e7acdc0d2092fd
skip <huangkai919@163.com>
2018-06-29 11:44:34 +0800

    add dup demo

---
+++ dup2.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"fmt"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, filePath := range files{
+			f, err := os.Open(filePath)
+			if err != nil{
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+			}else{
+				countLines(f, counts)
+			}
+			f.Close()
+		}
+	}
+	fmt.Printf("%v\n", counts)
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
